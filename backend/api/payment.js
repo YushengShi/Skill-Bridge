2a3efe3ba77b9f19e73a25fd5a9c82be247a9418
Yuhan <605286079@qq.com>
2025-12-12 12:27:17 -0500

    added backend route protection

--- backend/api/payment.js
+++ backend/api/payment.js
@@ -2,13 +2,14 @@ import "dotenv/config";
 import Stripe from "stripe";
 import { Router } from "express";
 import Booking from "../models/Booking.js";
+import protect from "../middleware/auth.js";
 
 const router = Router();
 
 // This is your test secret API key.
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-router.post("/create-checkout-session", async (req, res) => {
+router.post("/create-checkout-session", protect, async (req, res) => {
   // TODO: replace this url
   const YOUR_DOMAIN = "http://localhost:5173"; // replace with frontend domain
   const { teacherId, lessonType, price, teacherName } = req.body;
@@ -41,8 +42,8 @@ router.post("/create-checkout-session", async (req, res) => {
         bookingId: newBooking._id.toString()
       },
 
-      success_url: `${YOUR_DOMAIN}/teachers?success=true&bookingId=${newBooking._id}`,
-      cancel_url: `${YOUR_DOMAIN}/teachers?canceled=true`,
+      success_url: `${YOUR_DOMAIN}/teacherhome?success=true&bookingId=${newBooking._id}`,
+      cancel_url: `${YOUR_DOMAIN}/teacherhome?canceled=true`,
     });
 
     res.json({ url: session.url });
